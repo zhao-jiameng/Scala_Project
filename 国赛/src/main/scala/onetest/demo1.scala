31a7d6bf9bac11861517ba8785c982bc727e3d0c
zhao-jiameng <1273417644@qq.com>
2023-05-15 15:55:07 +0800

    first submit

---
+++ 国赛/src/main/scala/onetest/demo1.scala
@@ -0,0 +1,16 @@
+package onetest
+import
+/**
+ *
+ * @PROJECT_NAME: 国赛
+ * @PACKAGE_NAME: demo1
+ * @author: 赵嘉盟-HONOR
+ * @data: 2023-05-15 14:51
+ * @DESCRIPTION
+ *
+ */
+object demo1 {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
